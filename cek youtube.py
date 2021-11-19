69f7cc2c51279576756f7c2d0ad35b8caa782fc7
Dewa Gede Nata Raditya <nataradityaodit@gmail.com>
2021-11-19 18:51:15 +0700

    Update cek youtube.py

--- cek youtube.py
+++ cek youtube.py
@@ -7,45 +7,94 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
 from re import search
+import re
 import time
 import pandas as pd
 import gspread
 from random import randint
 
 gc = gspread.oauth()
+data = gc.open_by_url("https://docs.google.com/spreadsheets/d/1dUQxJn1fOIw9dx1weECAuw7fGzlrTA8cbZcq3dTBhPk/edit#gid=1458472153") #Link checkpoint yang ingin diubah
+ws_stream = data.worksheet("Streamyard") # disesuaikan dengan checkpoint terakhir
+vform = ws_stream.get_all_values()
+data = pd.DataFrame.from_records(vform, columns=vform[1])
+data= data[["Link YT - AKEM\n(Youtube)"]]
 
+data.drop(index=data.index[:2,], 
+        axis=0, 
+        inplace=True)
+# data.drop(index=data.index[5:], 
+#         axis=0, 
+#         inplace=True)
+
+profile_path = r'C:\Users\Acer\AppData\Roaming\Mozilla\Firefox\Profiles\51rk56w7.default'
 options=Options()
-options.profile = r'C:\Users\Acer\AppData\Roaming\Mozilla\Firefox\Profiles\51rk56w7.default'
+options.set_preference = ('profile', profile_path)
 options.headless = False
 service = Service(r'D:\Python YGB\Scraper\geckodriver.exe')
 
-link_youtube = []
+data_link = data.values.tolist()
+link_youtube= []
+for x in data_link:
+    for link in x:
+        link_youtube.append(link)
 status = []
+deskripsi = []
+thumbnail = []
+judul = []
+driver = webdriver.Firefox(options=options, service=service)
 for link in link_youtube:
-    driver = webdriver.Firefox(options=options, service=service)
-    driver.implicitly_wait(10)
-    driver.get(link)
-
     try:
-        element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="count"]/ytd-video-view-count-renderer/span[1]'))
-        )
-        live_stat = driver.find_element(By.XPATH, '//*[@id="count"]/ytd-video-view-count-renderer/span[1]').text
-        if search("waiting", str(live_stat)):
-            stat = "Belum tayang livestream"
-        elif search("views", str(live_stat)):
-            stat = "Sedang tayang"
-        elif search("watching", str(live_stat)):
-            stat = "Sedang livestream"
+        driver.get(link)
+        try:
+            element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, '//*[@id="info-strings"]/yt-formatted-string'))
+            ) #|(By.XPATH, '//*[@id="count"]/ytd-video-view-count-renderer/span[1]')
+            live_stat = driver.find_element(By.XPATH,'//*[@id="info-strings"]/yt-formatted-string').text
+            if search("Scheduled", str(live_stat)):
+                stat = "Belum memulai livestream"
+            elif search("Started", str(live_stat)):
+                stat = "Sedang livestream"
+            else:
+                stat = "Sudah menjadi video"
+            status.append(stat)
+            desk = driver.find_element(By.XPATH, '//*[@id="description"]/yt-formatted-string').text
+            title = driver.find_element(By.XPATH, '//*[@id="container"]/h1/yt-formatted-string').text
+            exp = "^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*"
+            s = re.findall(exp,link)[0][-1]
+            thumb = f"https://i.ytimg.com/vi/{s}/maxresdefault.jpg"
+            deskripsi.append(desk)
+            thumbnail.append(thumb)
+            judul.append(title)
+        except:
+            stat = 'Livestream youtube tidak ada'
+            status.append(stat)
+            desk = "Tidak ada deskripsi"
+            title = "Tidak ada judul"
+            thumb = "Tidak ada thumbnail"
+            deskripsi.append(desk)
+            thumbnail.append(thumb)
+            judul.append(title)
+    except:
+        stat = 'Tidak ada link youtube'
         status.append(stat)
-    finally:
-        driver.close()
-        time.sleep(randint(0,2))
-        # print(status)
+        desk = "Tidak ada deskripsi"
+        title = "Tidak ada judul"
+        thumb = "Tidak ada thumbnail"
+        deskripsi.append(desk)
+        thumbnail.append(thumb)
+        judul.append(title)
+driver.quit()
 status_pd = {
-    "Status Youtube" : status
+    "Status Youtube" : status,
+    "Deskripsi" : deskripsi,
+    "Judul" : judul,
+    "Gambar video" : thumbnail
 }
-status_pd = pd.DataFrame(status_pd)
+data_rapi = pd.DataFrame(status_pd)
+list_rapi = data_rapi.to_numpy().tolist()
+headers_rapi = data_rapi.columns.tolist()
+dataupdate = [headers_rapi] + list_rapi
 
-
+ws_stream.update("F2", dataupdate)
 print("Selesai")
